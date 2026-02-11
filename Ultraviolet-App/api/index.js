e272711672275458c1cbb8ac4f45ed11010ca5c3
YallSumLs <61993527+YallSumLs@users.noreply.github.com>
2026-02-11 18:45:07 -0500

    Update index.js

--- Ultraviolet-App/api/index.js
+++ Ultraviolet-App/api/index.js
@@ -1,1 +1,31 @@
+import { join } from "node:path";
+import { hostname } from "node:os";
+import express from "express";
+import { uvPath } from "@titaniumnetwork-dev/ultraviolet";
+import { epoxyPath } from "@mercuryworkshop/epoxy-transport";
+import { baremuxPath } from "@mercuryworkshop/bare-mux/node";
 
+const app = express();
+
+// CORS headers middleware
+app.use((req, res, next) => {
+  res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+  res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+  next();
+});
+
+// Load our publicPath first and prioritize it over UV.
+app.use(express.static("./public"));
+// Load vendor files last.
+app.use("/uv/", express.static(uvPath));
+app.use("/epoxy/", express.static(epoxyPath));
+app.use("/baremux/", express.static(baremuxPath));
+
+// Error for everything else
+app.use((req, res) => {
+  res.status(404);
+  res.sendFile(join(process.cwd(), "./public/404.html"));
+});
+
+// Export for Vercel
+export default app;
